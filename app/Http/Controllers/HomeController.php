3c2c38ee5ae9c9923cbbee12d187646a6feb463d
LongDev9999 <83637938+LongDev9999@users.noreply.github.com>
2021-06-07 22:43:38 +0700

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+<<<<<<< HEAD
 
 class HomeController extends Controller
 {
@@ -25,4 +26,36 @@ class HomeController extends Controller
     {
         return view('home');
     }
+=======
+use Illuminate\Support\Facades\Auth;
+
+
+
+class HomeController extends Controller
+{
+    public function getShop()
+    {
+        return view('home_pages.shop');
+    }
+
+    public function getNews(){
+        return view('home_pages.news');
+    }
+    public function getAbout(){
+        return view('home_pages.about');
+    }
+
+    public function contactUs(){
+        return view('home_pages.contact_us');
+    }
+
+    public function productDetails(){
+        return view('home_pages.product_details');
+    }
+
+    public function newsDetails(){
+        return view('home_pages.news_details');
+    }
+
+>>>>>>> ba4e2a08cc1db7a7d11869b1b3908dd26fdc9ed0
 }
